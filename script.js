dfb30002a3b4796520330565e12bdfcae9ed3992
Abdul Samad Maan <167423682+abdulsamadmaan@users.noreply.github.com>
2024-09-05 10:42:44 +0000

    Functions Examples

--- script.js
+++ script.js
@@ -21,6 +21,29 @@
 
 // Create Function for the increment
 
-function increment(){
-    console.log("Button is clicked")
-}
+// function increment(){
+//    // console.log("Button is clicked")
+// }
+
+// function number(){
+//     const num = 42;
+//     // console.log(num)
+// }
+
+// number()
+
+// function laps(){
+
+//     let lap1 = 34
+//     let lap2 = 10
+//     let lap3 = 20
+
+//     result = lap1 + lap2+ lap3
+
+//     console.log (result)
+
+// }
+
+// laps()
+
+
