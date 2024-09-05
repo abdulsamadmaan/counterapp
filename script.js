af54294cef39b57c8b4d9f9ae5b693563a9555fe
Abdul Samad Maan <167423682+abdulsamadmaan@users.noreply.github.com>
2024-09-05 09:06:15 +0000

    Counter App Button and H1 onClick

---
+++ script.js
@@ -0,0 +1,26 @@
+//document.getElementById("count-el").ineertext = 5
+
+// myAge = 23
+//console.log(myAge)
+
+//let humanage = 3
+//let avgmydogage = 2
+//mydogage = humanage * avgmydogage
+//console.log(mydogage)
+
+//let count = 50
+//count = count + 50
+//console.log(count)
+
+//let count1= count = count - 25
+//console.log(count)
+
+// let count2 = count - 5
+// console.log (count2)
+
+
+// Create Function for the increment
+
+function increment(){
+    console.log("Button is clicked")
+}
